09dd3dceb1e1b21a2fadea5eb105812295daaeb4
ruchitakb7 <ruchitakb7@gmail.com>
2026-07-15 17:24:43 +0530

    routing based on token

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 
 import LoginPage from "@/component/auth/login";
+import { cookies } from "next/headers";
+import {redirect} from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+
+  const token = (await cookies()).get("auth_token");
+  console.log(token, "token");
+
+  if (token) {
+    redirect("/dashboard");
+  }
+
   return <LoginPage />;
 }
